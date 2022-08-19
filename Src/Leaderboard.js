744292171828a2aa2280ce1e99c6f6b661e8e0fc
Alan Hu <64699846+Ailun-Hu@users.noreply.github.com>
2022-08-19 13:56:16 -0400

    add leaderboard feature

---
+++ Src/Leaderboard.js
@@ -0,0 +1,5 @@
+
+window.onload = () => {
+    let node = document.createElement('li').appendChild(document.createTextNode("test"));
+    document.getElementById("leaderboard").appendChild(node);
+} 
